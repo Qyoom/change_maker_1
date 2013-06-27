03e2addfb153b4feb8b2fb140fa1ccb414e501ef
Richard Walker <richard@aqume.com>
2013-06-26 23:48:05 -0700

    Adding view model knockout code...

--- scripts/change-maker-1.0.js
+++ scripts/change-maker-1.0.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-	console.log("The DOM is now loaded and can be manipulated.");
+	console.log("The DOM is now loaded.");
+	
+	// Overall viewmodel for this screen, along with initial state
+	function ChangeMakerViewModel() {
+		var self = this;
+	}
+	
+	ko.applyBindings(new ChangeMakerViewModel());
 });
