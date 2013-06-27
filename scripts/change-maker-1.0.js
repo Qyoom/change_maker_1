44e7b730df30af8cc72ca47f0011499168dee267
Richard Walker <richard@aqume.com>
2013-06-27 01:39:19 -0700

    Monetary value dislaying properly in a field ... on to all of them...

--- scripts/change-maker-1.0.js
+++ scripts/change-maker-1.0.js
@@ -3,7 +3,17 @@ $(document).ready(function () {
 	
 	// Overall viewmodel for this screen, along with initial state
 	function ChangeMakerViewModel() {
+		console.log("ChangeMakerViewModel TOP");
 		var self = this;
+		this.cd_twenties = ko.observable();
+		
+		// Computed data
+		self.amtCdTwenties = ko.computed(function() {
+			console.log("self.amtCdTwenties TOP - self.cd_twenties(): " + self.cd_twenties());
+		   	var amt = self.cd_twenties();
+			console.log("typeof amt: " + (typeof amt));
+		   	return amt === undefined ? 0.0 : parseFloat(amt);
+		});
 	}
 	
 	ko.applyBindings(new ChangeMakerViewModel());
