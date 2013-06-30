c421810e46bddb67916e9cd872288b0356f1efad
Richard Walker <richard@aqume.com>
2013-06-30 16:31:17 -0700

    Fixed rounding error on changeBalRemain

--- scripts/change-maker-2-purchase.js
+++ scripts/change-maker-2-purchase.js
@@ -6,15 +6,15 @@ function processPayment(e) {
 	e.preventDefault();
 	console.log("processPayment TOP");
 	
-	var cost = formatCurrencyCalc($("#cost").val());
-	var payment = formatCurrencyCalc($("#payment").val());
+	var cost = formatCurrencyCalcStr($("#cost").val());
+	var payment = formatCurrencyCalcStr($("#payment").val());
 	
 	if(payment < cost) {
 		$('#error').text('Payment is less than the cost.');
 		return;
 	}
 	
-	var changeDue = (payment >= cost) ? (Math.round((payment - cost)*100)/100) : 0.00; // Round floats to two decimal places
+	var changeDue = (payment >= cost) ? roundCurrencyCalcNum(payment - cost) : 0.00; // Round floats to two decimal places
 	
 	// Accumulators	
 	var changeBalRemain = changeDue;
@@ -25,29 +25,38 @@ function processPayment(e) {
 	$('#cash-drawer li').each(function(index) {
 		// "this" is the object of the current iteration
 		var name = $(this).children("label:first").text(); // name <- denomination name
-		console.log("name: " + name);
+		console.log("---------->denomination: " + name);
 		var denomQty = parseInt($(this).children("input:first").val());
-		var denomVal = formatCurrencyCalc($(this).children("input:first").attr("data-denom"));
+		console.log("denomQty: " + denomQty);
+		var denomVal = formatCurrencyCalcStr($(this).children("input:first").attr("data-denom"));
+		console.log("denomVal: " + denomVal);
 		// Determine how many times this denomination can fit into the remaining balance
+		changeBalRemain = roundCurrencyCalcNum(changeBalRemain);
 		var denomFactor = (Math.floor(changeBalRemain / denomVal));
+		console.log("denomFactor: " + denomFactor);
 		
 		// Factor and accumulate quantities and amounts - basic idea: denomination factor * denomination value
 		if(denomFactor > 0 && denomQty > 0) {
 			// Determine how many of this denomination are available
 			denomFactor = (denomFactor <= denomQty) ? denomFactor : denomQty;
+			console.log("denomFactor: " + denomFactor);
 			changeBalRemain = changeBalRemain - (denomVal * denomFactor);
+			console.log("changeBalRemain: " + changeBalRemain);
 			// store results for this denomination
 			changeInDenoms.push([name, denomFactor]);
 			changeAccume = changeAccume + (denomFactor * denomVal);
+			console.log("changeAccume: " + changeAccume);
 		}
 	});
 	
-	// UI display
+	//changeBalRemain = roundCurrencyCalcNum(changeBalRemain);
 	console.log("changeDue: " + changeDue + " | changeInDenoms: " + changeInDenoms + 
-		" | changeBalRemain < 0.01 ? " + (changeBalRemain < 0.01) + " | changeAccume: " + changeAccume);
-	$('#changeInDenoms').text(stringifyChange(changeInDenoms));
+		" | changeBalRemain (" + changeBalRemain + ") < 0.01 ? " + (changeBalRemain < 0.01) + " | changeAccume: " + changeAccume);
+	// UI display
+	$('#changeInDenoms').text(stringifyChange(changeInDenoms, changeAccume));
 	$('#chgDue').text("Change due: " + formatCurrencyPrint(changeDue));
-	if(changeBalRemain >= 0.01) {
+	// Determine is store drawer has enough to make change
+	if((roundCurrencyCalcNum(changeBalRemain)) >= 0.01) {
 		$('#error').text("Unable to process transaction‒Not enough change!");
 	}
 	else {
@@ -58,26 +67,33 @@ function processPayment(e) {
 	$("#payment").val(formatCurrencyPrint(payment));
 }
 
+// Maintain two decimal places for float calc
+function roundCurrencyCalcNum(num) {
+	var result = Math.round(parseFloat(num) * 100) / 100;
+	return result;
+}
+
 // Maintain two decimal place formatting for floats from strings
-function formatCurrencyCalc(str) {
+function formatCurrencyCalcStr(str) {
 	var result = Math.round(parseFloat(str) * 100) / 100;
 	return result;
 }
 
+// Format currency for printing
 function formatCurrencyPrint(num) {
     num = isNaN(num) || num === '' || num === null ? 0.00 : num;
     return parseFloat(num).toFixed(2);
 }
 
 // Convert collected data to readable message
-function stringifyChange(change) {
+function stringifyChange(changeAccumeDenoms, changeAccumeSum) {
 	var result = "";
-	if(change.length == 0) return result;
-	else result = result.concat("Change returned: ");
+	if(changeAccumeDenoms.length == 0) return result;
+	else result = result.concat("Change returned (" + formatCurrencyPrint(changeAccumeSum) + "): ");
 	var denom = 0, qty = 1;
-	for(var i = 0; i < change.length; i++) {
-		result = result.concat(change[i][denom], ": ", change[i][qty]);
-		if(i < change.length - 1) result = result.concat(", ");
+	for(var i = 0; i < changeAccumeDenoms.length; i++) {
+		result = result.concat(changeAccumeDenoms[i][denom], ": ", changeAccumeDenoms[i][qty]);
+		if(i < changeAccumeDenoms.length - 1) result = result.concat(", ");
 	}
 	return result;
 }
